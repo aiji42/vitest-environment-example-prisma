c2b7250f7f9c3d4ee4f05ae9c25cee8a0f877ca5
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-11-16 22:54:03 +0900

    update test case

--- prisma.test.ts
+++ prisma.test.ts
@@ -18,6 +18,40 @@ describe("User", () => {
     ).toStrictEqual(createdUser);
   });
 
+  test("Add users on transaction", async () => {
+    const [user1, user2] = await prisma.$transaction(async (t) => {
+      const user1 = await t.user.create({
+        data: {
+          nickname: "user1",
+          email: "user1@example.com",
+        },
+      });
+      const user2 = await t.user.create({
+        data: {
+          nickname: "user2",
+          email: "user2@example.com",
+        },
+      });
+      return [user1, user2];
+    });
+
+    expect(
+      await prisma.user.findFirst({
+        where: {
+          nickname: "user1",
+        },
+      })
+    ).toStrictEqual(user1);
+
+    expect(
+      await prisma.user.findFirst({
+        where: {
+          nickname: "user2",
+        },
+      })
+    ).toStrictEqual(user2);
+  });
+
   test("Count user", async () => {
     expect(await prisma.user.count()).toBe(0);
   });
